921b14d32b0460e3bbcd92f65da9b6e4e6fa91e2
ErnestKyei <kyeiernest86@gmail.com>
2026-03-01 12:52:39 +0000

    building

--- backend-api/routes/authRoute.js
+++ backend-api/routes/authRoute.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
-const protect = require('../middlewares/authMiddleware')
-
-
+const protect = require('../middlewares/authMiddleware');
+const rateLimit = require('express-rate-limit');
 
 
 
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+const authLimiter = rateLimit({
+        windowMs: 15 * 60 * 1000,
+        max: 10,
+        message: {
+            success: false,
+            message:"Too many attempt, please try again  after"
+        }
+
+
+})
+
+
+
+
+
+
+router.post('/register',authLimiter, authController.register);
+router.post('/login', authLimiter,authController.login);
 router.get('/logout',protect, authController.logout);
 
 
